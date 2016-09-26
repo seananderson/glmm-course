436ff03f530f6109d042d8a39d01d2764065d4df
Sean Anderson <sean@seananderson.ca>
2016-09-26 16:41:14 -0700

    Also remove markdown exercise comments

--- 99-make.R
+++ 99-make.R
@@ -3,6 +3,7 @@ dir.create("exercises", showWarnings = FALSE)
 remove_exercises <- function(x) {
   f <- readLines(x)
   f_ex <- ifelse(grepl("# exercise", f), "# exercise", f)
+  f_ex <- ifelse(grepl("<!-- exercise -->", f_ex), "<!-- exercise -->", f_ex)
   writeLines(as.character(f_ex), con = file.path("exercises", x))
 }
 exercises <- lapply(files, remove_exercises)
