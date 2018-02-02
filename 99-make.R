f77d1c16f0a50a9d1222529c2d2a2af8b2cd60c0
Sean Anderson <sean@seananderson.ca>
2018-02-01 21:34:15 -0800

    Fix knitting

--- 99-make.R
+++ 99-make.R
@@ -6,13 +6,7 @@ remove_exercises <- function(x) {
   f_ex <- ifelse(grepl("<!-- exercise -->", f_ex), "<!-- exercise -->", f_ex)
   writeLines(as.character(f_ex), con = file.path("exercises", x))
 }
-exercises <- lapply(files, remove_exercises)
+purrr::walk(files, remove_exercises)
 
-# knit all exercises
-
-knit_exercises <- function(x) {
-  rmarkdown::render(x, "github_document")
-}
-#ignore_me <- lapply(files, knit_exercises)
-
-rmarkdown::render("00-schedule.Rmd", "github_document")
+## knit all exercises (slow)
+purrr::walk(files, rmarkdown::render)
